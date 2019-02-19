8fb07b6abe64feecbf77d3e41005a10a8a240d18
tusharhirpara <tusharhirpara11@gmail.com>
2019-02-19 13:48:42 +0530

    Update try.js

--- try.js
+++ try.js
@@ -66,4 +66,20 @@ number.forEach(function (param,k,ar) {
     }
     console.log(ar[k]);
 });
+
+
+var ar=[50,45,2,15,18,17];
+var c=ar.map(function (param,k,ar) {
+    for(var i=0;i<ar.length;i++){
+        for(var j=i+1;j<ar.length;j++){
+            if(ar[j]<ar[i]){
+                temp=ar[j];
+                ar[j]=ar[i];
+                ar[i]=temp;
+            }
+        }
+    }
+    return ar[k];
+});
+console.log(c);
  
